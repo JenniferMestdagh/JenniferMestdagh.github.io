f7f6d31d84b1a8d304994bdecac6de5ee11547f2
JenniferMestdagh <56117697+JenniferMestdagh@users.noreply.github.com>
2022-03-15 13:53:39 +0100

    laatste bijwerkingen

--- JavaScript/Labo 3/Javascript/colorpicker.js
+++ JavaScript/Labo 3/Javascript/colorpicker.js
@@ -8,12 +8,18 @@ const setup = () => {
 
 const update = () => {
     const sliders = document.getElementsByClassName("slider");
+    let labels = document.getElementsByTagName("label");
     let kleurRood = sliders[0].value;
     let kleurGroen = sliders[1].value;
     let kleurBlauw = sliders[2].value;
 
     let hokje = document.getElementById("hokje");
-    hokje.style.backgroundColor=
+
+    labels[0].textContent = 'red: ' + kleurRood;
+    labels[1].textContent = 'green: ' + kleurGroen;
+    labels[2].textContent = 'blue: ' + kleurBlauw;
+
+    hokje.style.backgroundColor= `rgb(${kleurRood},${kleurGroen}, ${kleurBlauw})`
 }
 
 window.addEventListener("load", setup);
--- JavaScript/Labo 3/Javascript/producten.js
+++ JavaScript/Labo 3/Javascript/producten.js
@@ -1,3 +1,27 @@
 const setup = () => {
+
+    let knop = document.getElementById("knop");
+    knop.addEventListener("click", berekenBedrag)
+
 }
+const berekenBedrag = () => {
+    let prijzen = document.getElementsByClassName("prijs");
+    let input = document.getElementsByTagName("input");
+    let btw = document.getElementsByClassName("btw");
+    let subtotaal = document.getElementsByClassName("subtotaal");
+    let totaal = document.getElementById("totaal");
+
+    // omzeten in number van String
+    let subtotaal1 = parseFloat(prijzen[0].textContent) + parseFloat(prijzen[0].textContent) * parseFloat(input[0].value) * parseInt(btw[0].textContent) / 100;
+    subtotaal[0].textContent = subtotaal1.toFixed(2);
+
+    let subtotaal2 = parseFloat(prijzen[1].textContent) + parseFloat(prijzen[0].textContent) * parseFloat(input[1].value) * parseInt(btw[1].textContent) / 100;
+    subtotaal[1].textContent = subtotaal2.toFixed(2);
+
+    let subtotaal3 = parseFloat(prijzen[2].textContent) + parseFloat(prijzen[0].textContent) * parseFloat(input[2].value) * parseInt(btw[2].textContent) / 100;
+    subtotaal[2].textContent = subtotaal3.toFixed();
+
+    totaal.textContent = (subtotaal1 + subtotaal2 + subtotaal3).toFixed(2);
+}
+
 window.addEventListener("load", setup);
