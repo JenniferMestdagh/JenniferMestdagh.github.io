2df689d59416aa6814368108f8fd9e0b287e87a3
JenniferMestdagh <56117697+JenniferMestdagh@users.noreply.github.com>
2022-03-29 20:09:08 +0200

    add formvalidate files

--- JavaScript/Labo 5/Javascript/formvalidattie.js
+++ JavaScript/Labo 5/Javascript/formvalidattie.js
@@ -1,3 +1,29 @@
 const setup = () => {
+    email.addEventListener("input", emailValidation);
+    children.addEventListener("input", childrenValidation);
 }
-window.addEventListener("load", setup);
+
+const email = document.getElementById("email");
+const emailValidation = () => {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("Geen geldig e-mail adres");
+        email.reportValidity();
+    } else {
+        email.setCustomValidity("");
+    }
+}
+
+const children = document.getElementById("kinderen");
+const childrenValidation = () => {
+    if (children.value < 0) {
+        children.setCustomValidity("Is geen positief getal!");
+        children.reportValidity();
+    } else if (children.value > 99) {
+        children.setCustomValidity("Is te vruchtbaar")
+    } else {
+        email.setCustomValidity("");
+    }
+}
+
+let button = document.getElementById('button')
+button.addEventListener("click", setup);
