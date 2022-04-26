8dc0b97d9bb7f264df3b667ee57e9ff738ca5f74
JenniferMestdagh <56117697+JenniferMestdagh@users.noreply.github.com>
2022-04-26 13:30:28 +0200

    labo 7
    afgewerkt

--- JavaScript/Labo 7/MatchingGame/scripts/main.js
+++ JavaScript/Labo 7/MatchingGame/scripts/main.js
@@ -1,30 +1,24 @@
-let AANTAL_HORIZONTAAL=4;
-let AANTAL_VERTICAAL=3;
-let AANTAL_KAARTEN=6;
-let arr = ['kaart1.png', 'kaart2.png', 'kaart3.png', 'kaart4.png', 'kaart5.png', 'kaart6.png'];
+let arr = ['kaart1.png', 'kaart2.png', 'kaart3.png', 'kaart4.png', 'kaart5.png', 'kaart6.png', 'kaart1.png', 'kaart2.png', 'kaart3.png', 'kaart4.png', 'kaart5.png', 'kaart6.png'];
 let geselecteerdeKaarten = 0;
 
 const setup = () => {
-    genereerKaartjes();
+    genereerSpelbord();
     setTimeout(verbergKaartjes, 5000);
 }
 
-const genereerKaartjes = () => {
-    let randomNumber = Math.ceil(Math.random() * 6);
-    let playField = document.getElementById('playField');
+const genereerKaartjes = (i) => {
+    let card = document.createElement("img");
+    card.classList.add("card");
 
-    for (let i = 0; i < AANTAL_KAARTEN; i++){
-        let card = document.createElement('img');
+    card.addEventListener("click", selecteer);
+    return card;
+}
+
+const genereerSpelbord = () => {
+    for(let i = 0; i < 12; i++){
+        let card = genereerKaartjes(i);
+        document.getElementById("playField").appendChild(card);
         card.src = 'images/' + arr[i];
-        playField.appendChild(card);
-        card.className = "card";
-        card.setAttribute("data-card",'images/' + arr[i]);
-
-        let card2 = document.createElement('img');
-        card2.src = 'images/' + arr[i];
-        playField.appendChild(card2);
-        card2.className = "card";
-        card2.setAttribute("data-card",'images/' + arr[i]);
     }
 }
 
@@ -37,7 +31,7 @@ const verbergKaartjes = () => {
 }
 
 const selecteer = (f) => {
-    let clickedCard = event.target;
+    let clickedCard = f.target;
     clickedCard.src = clickedCard.getAttribute("data-card");
     clickedCard.classList.toggle("geselecteerd")
 
